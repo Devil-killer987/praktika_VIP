c71042e637498de0f9fffc6dba69502205b91c43
Devil-killer987 <aartaym.voldit.20075@gmail.com>
2026-05-04 13:19:53 +0300

    Update History.xaml.cs

--- 5 день/интересная прога/основное прилож/History.xaml.cs
+++ 5 день/интересная прога/основное прилож/History.xaml.cs
@@ -5,21 +5,26 @@ namespace Master_floor
 {
     public partial class History : Window
     {
-        TestBaseEntities db = new TestBaseEntities();
+        private TestBaseEntities databaseContext = new TestBaseEntities();
 
-        public History(int partnerId)
+        public History(int partnerIdentifier)
         {
             InitializeComponent();
-            var data = db.Partners_product
-                .Where(pp => pp.ID_Partner == partnerId)
-                .Select(pp => new
+            LoadSalesHistory(partnerIdentifier);
+        }
+
+        private void LoadSalesHistory(int partnerIdentifier)
+        {
+            var salesData = databaseContext.Partners_product
+                .Where(sale => sale.ID_Partner == partnerIdentifier)
+                .Select(sale => new
                 {
-                    ProductName = pp.Product.Наименование_продукции,
-                    Count = pp.Количество_продукции,
-                    Date = pp.Дата_продажи
+                    ProductName = sale.Product.Наименование_продукции,
+                    Quantity = sale.Количество_продукции,
+                    SaleDate = sale.Дата_продажи
                 }).ToList();
 
-            dgHistory.ItemsSource = data;
+            dgHistory.ItemsSource = salesData;
         }
     }
 }
